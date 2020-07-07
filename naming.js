9ca3ac2a0dba8bc2ba689316d6f7a1fde12e94f2
mohammedhilfi <momohilfi03@Gmail.com>
2020-07-07 14:19:18 -0700

    added varablies

---
+++ naming.js
@@ -0,0 +1,20 @@
+let dog = 11 
+let cat = 'dog' 
+let ball = 28
+let bed = 32
+let school = 3
+const work = "school"
+const hello = "cat"
+const water = "ball"
+const phone = "charger"
+const sun = "wind"
+console.log(dog)
+console.log(cat)
+console.log(ball)
+console.log(bed)
+console.log(school)
+console.log(work)
+console.log(hello)
+console.log(water)
+console.log(phone)
+console.log(sun)
